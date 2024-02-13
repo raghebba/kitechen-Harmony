f7634675eece969981259d69985a070668ab00f0
raghebba <ragheb.ben.amor@outlook.com>
2024-02-13 20:06:15 +0100

    adding adjust the header adding logo adding Some SEO optomiation

--- src/component/UI/Header.js
+++ src/component/UI/Header.js
@@ -1,21 +1,32 @@
 import React from "react";
 import { useAppContext } from "../../contextAPI/languageContext";
-import "./header.module.css"
+import  "./header.module.css";
 
-
-const Header = () =>{
-    const {selectedLanguage} = useAppContext()
-    return(
-        <header>
-            {selectedLanguage === "tunisian" ?
-            (<h1>توانسة هارموني</h1>):
-            selectedLanguage === "french" ? 
-            (<h1>Harmonie en Cuisine</h1>):
-            (<h1>Kitchen Harmony</h1>)
-            }
-        
-    </header>
-    )
+const Styles = {
+    logo: {
+        width: '50px', // Set a small width for the logo; adjust as needed
+        height: 'auto', // Maintain aspect ratio
+      },
 }
 
-export default Header
+const Header = () => {
+  const { selectedLanguage } = useAppContext();
+  return (
+    <header>
+      {selectedLanguage === "tunisian" ? (
+        <h1> توانسة هارموني <br /> Beta </h1>
+      ) : selectedLanguage === "french" ? (
+        <h1>Harmonie en Cuisine <br /> Beta</h1>
+      ) : (
+        <h1>Kitchen Harmony  <br /> Beta</h1>
+      )}
+      <img
+        style={Styles.logo}
+        src={`${process.env.PUBLIC_URL}/logo.png`}
+        alt="logo"
+      />
+    </header>
+  );
+};
+
+export default Header;
